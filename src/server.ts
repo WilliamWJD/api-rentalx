eed33c15e01b8e202beacb53666d81bf68e28a7e
williamwjd <william.jose.wjd@gmail.com>
2021-03-23 20:53:21 -0300

    configurando o debbug

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from "express";
 
-const server = express();
+const app = express();
 
-server.listen(3333, () => {
+app.use(express.json());
+
+app.post("/courses", (req, res) => {
+    const { name } = req.body;
+    return res.json(name);
+});
+
+app.listen(3333, () => {
     console.log("Servidor online");
 });
