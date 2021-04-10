b74569fe5ec65979fb52ab571f465e4abb6bc8f7
williamwjd <william.jose.wjd@gmail.com>
2021-04-10 11:58:48 -0300

    Refatorando arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
 
-import { categorieRoutes } from "./routes/categories.routes";
-import specificationsRoutes from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/categories", categorieRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => {
     console.log("Servidor online");
