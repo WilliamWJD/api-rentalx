341b3790c3349fe1880ccecd6c79ccd11a621c05
williamwjd <william.jose.wjd@gmail.com>
2021-06-01 06:18:34 -0300

    refatorando o repository de categorias

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryColumn, CreateDateColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
 
 @Entity("categories")
 class Category {
@@ -13,6 +14,12 @@ class Category {
 
     @CreateDateColumn()
     created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
 }
 
 export { Category };
