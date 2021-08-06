46822a8d2fab137a647939d66f1c3cabb3c1d775
williamwjd <william.jose.wjd@gmail.com>
2021-08-05 21:21:34 -0300

    criando seed para criação do usuário administrador

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,7 @@
-import { createConnection } from "typeorm";
+import { Connection, createConnection } from "typeorm";
 
 createConnection();
+
+export default async (): Promise<Connection> => {
+    return createConnection();
+};
